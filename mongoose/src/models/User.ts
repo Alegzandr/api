ee371269ba4bd0e0276f15c42ecfafeef611d163
Alexandre Farrenq <alexandre1.farrenq@epitech.eu>
2023-06-11 14:07:19 +0200

    Add User schema

---
+++ mongoose/src/models/User.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [
+            /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+            'Please fill a valid email address',
+        ],
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        minlength: 5,
+        maxlength: 20,
+    },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, required: true },
+    updatedAt: { type: Date, default: Date.now, required: true },
+});
+
+userSchema.pre('save', function (next) {
+    if (!this.isNew) {
+        this.updatedAt = new Date();
+    }
+    next();
+});
+
+export default mongoose.model('User', userSchema);
